bd758e2fdf32c82e386f5222e6c00eba7d2a5bd3
Fokion Zervoudakis <fokion.zervoudakis@gmail.com>
2019-05-21 23:07:39 +0100

    Update Javadoc.

--- src/misc/BoyerM.java
+++ src/misc/BoyerM.java
@@ -2,16 +2,18 @@ package misc;
 
 class BoyerM {
     /**
-     Uses the Boyer-Moore majority vote algorithm to find the majority element in
-     an array of integers
+     Uses the Boyer-Moore majority vote algorithm to find the majority element
+     (i.e. the element that appears more than {@code n/2} times) in an array of
+     {@code n} integers. The algorithm will report one of the elements even if
+     the array contains no majority element. A second pass could be used to
+     determine whether the element returned is the majority element.
      <p>Asymptotic analysis:
      <ul>
      <li>time_worst=O(n)
      <li>space_worst=O(1)
      </ul>
 
-     @param A an array of {@code n} integers containing a majority element that
-     appears more than {@code n/2} times
+     @param A an array of integers
      @return the majority element of {@code A}
      */
     Integer majorityVote(int[] A) {
